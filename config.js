2b14ff72a7739ec91c60ab8d3ace7a6df74c0151
xiao7511 <xiao.ye751111@outlook.com>
2026-04-29 22:29:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // GitHub Actions overwrites this file at deploy time using GitHub Secrets.
 // Never put a Supabase service_role key here.
 window.APP_CONFIG = window.APP_CONFIG || {
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: ''
+  SUPABASE_URL: 'https://kogjjfccyncdszuuwlun.supabase.co/rest/v1/',
+  SUPABASE_ANON_KEY: 'sb_publishable_sUhEUYYHIr7hQl6A_iE4zQ_F-P3KENr'
 };
